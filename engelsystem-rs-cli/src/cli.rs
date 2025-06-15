842314d96023930960d869403eb2eebf7137bb68
willow <52585984+Kek5chen@users.noreply.github.com>
2025-06-15 17:34:36 +0200

    document cli functions

--- engelsystem-rs-cli/src/cli.rs
+++ engelsystem-rs-cli/src/cli.rs
@@ -3,7 +3,7 @@ use engelsystem_rs_db::role::RoleType;
 
 #[derive(Debug, Parser)]
 #[command(name = "engelcli")]
-#[command(about = "Cli for administratively managing the engelsystem and its data")]
+#[command(about = "Interface for administratively managing the engelsystem and its data")]
 pub enum EngelCli {
     #[command(subcommand)]
     Users(UsersCmd),
@@ -13,13 +13,16 @@ pub enum EngelCli {
 }
 
 #[derive(Debug, Subcommand)]
+#[command(about = "User related management commands")]
 pub enum UsersCmd {
+    #[command(about = "List all users")]
     List,
 
     Role(RoleCmd)
 }
 
 #[derive(Debug, Args)]
+#[command(about = "User role related management commands")]
 pub struct RoleCmd {
     pub user: String,
     
@@ -29,14 +32,18 @@ pub struct RoleCmd {
 
 #[derive(Debug, Subcommand)]
 pub enum RoleAction {
+    #[command(about = "Set <USER>s role to <ROLE>")]
     Set {
+        #[arg(help = "The new role of <USER>. This is dynamic, but default roles are 'Guest', 'User' and 'Admin'")]
         role: RoleType,
     }
 }
 
 
 #[derive(Debug, Subcommand)]
+#[command(about = "Debugging related commands")]
 pub enum DebugCmd {
+    #[command(about = "Create dummy users in the database")]
     CreateDummyUsers {
         amount: u32,
     }
